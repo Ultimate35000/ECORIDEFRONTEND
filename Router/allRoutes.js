8ba283c4a292212e4436064f7f978c1ad9abe0eb
Ultimate35000 <njorez@yahoo.fr>
2025-02-12 11:31:11 +0100

    MAJ Home

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,9 @@
 import Route from "./Route.js";
 
-//Définir ici vos routes
+//Définir ici nos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Ecoride";
