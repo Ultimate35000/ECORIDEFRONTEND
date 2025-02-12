5a20209ea89c52bc1ec40dd8abaea96f0f77d166
Ultimate35000 <njorez@yahoo.fr>
2025-02-12 14:32:00 +0100

    Add connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici nos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/connexion", "Page de connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
